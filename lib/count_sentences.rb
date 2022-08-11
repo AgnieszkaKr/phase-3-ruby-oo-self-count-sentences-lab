d04187ccb72b2ffdc0479620f5f3a85953e37e37
Agnes <agsawicka@gmail.com>
2022-08-10 22:50:11 -0400

    commit tree 22399c4ecbbb063d865efb996f5bb76e3ba256c7

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      true
+    else
+      false
+    end
   end
 
   def question?
+    if self[-1] == "?"
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
+    if self[-1] == "!"
+      true
+    else
+      false
+    end
 
   end
 
   def count_sentences
-
+    num_sentence =[]
+    arr = self.split
+    arr.each do |el|
+      if el[-1] == "." || el[-1] == "?" ||el[-1] == "!" 
+        num_sentence <<el
+      end
+    end
+    num_sentence.length
   end
 end
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo..."
